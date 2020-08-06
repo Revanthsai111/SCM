82e66f1f02ce531550580939483f3c98fa18c7f9
Revanth <revanth.alampally@vitap.ac.in>
2020-08-06 11:15:47 +0000

    Initilization

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,3 +1,4 @@
+#hello I am Revanth
 #!/bin/bash
 
 echo "Enter your lucky number"
@@ -5,14 +6,14 @@ read n
 
 if [ $n -eq 101 ];
 then
-echo "You got 1st prize"
+	echo "You got 1st prize"
 elif [ $n -eq 510 ];
 then
-echo "You got 2nd prize"
+	echo "You got 2nd prize"
 elif [ $n -eq 999 ];
 then
-echo "You got 3rd prize"
+	echo "You got 3rd prize"
 
 else
-echo "Sorry, try for the next time"
+	echo "Sorry, try for the next time"
 fi
